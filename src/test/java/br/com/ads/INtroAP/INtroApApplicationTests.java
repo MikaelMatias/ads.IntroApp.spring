1de49c4d1054ff57bafbb6c5497d49551827176d
MIkael D P Matias <mikaeldavid461@gmail.com>
2022-09-20 10:20:59 -0300

    firstcommit

---
+++ src/test/java/br/com/ads/INtroAP/INtroApApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ads.INtroAP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class INtroApApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
